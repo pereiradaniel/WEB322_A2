de9b4a707bf13327d827124c4b5383ef289cba60
pereiradaniel <danielpaulpereira@protonmail.com>
2020-10-28 16:09:43 -0400

    Initial commit

---
+++ js/fooditems.js
@@ -0,0 +1,114 @@
+var items = [
+    {
+        "name": "Spaghetti con vongole",
+        "price": 16,
+        "imgName": "vongole",
+        "topPackage": true
+    },
+    {
+        "name": "Pasta alle sarde",
+        "price": 13,
+        "imgName": "sarde",
+        "topPackage": true
+    },
+    {
+        "name": "Pesce spada arrosto",
+        "price": 20,
+        "imgName": "spada",
+        "topPackage": true
+    },
+    {
+        "name": "Pasta alla Norma",
+        "price": 15,
+        "imgName": "norma",
+        "topPackage": true
+    }
+    {
+        "name": "Roast beef",
+        "price": 22,
+        "imgName": "beef",
+        "topPackage": false
+    },
+    {
+        "name": "Grilled cheese",
+        "price": 5,
+        "imgName": "cheese",
+        "topPackage": false
+    },
+    {
+        "name": "Hamburger",
+        "price": 10,
+        "imgName": "hamburger",
+        "topPackage": false
+    },
+    {
+        "name": "Bacon and eggs",
+        "price": 13,
+        "imgName": "baconeggs",
+        "topPackage": false
+    }
+]
+
+window.addEventListener("load", function() {
+    console.log("loaded");
+    
+    topMealPackages = document.getElementById("top-meal-packages");
+    
+    // Add top meal packages to page
+    for (var i = 0; i < items.length; i++) {
+        
+        if (items[i].topPackage === true) {
+            // Create new div for food item
+            var currentItem = document.createElement("div");
+
+            // Add image
+            var itemImg = document.createElement("img");
+            itemImg.src = items[i].imgName + ".jpg";
+            itemImg.classList.add("item-image");
+            currentItem.appendChild(itemImg);
+
+            // Add name
+            var nameField = document.createElement('h3');
+            var itemName = document.createTextNode(items[i].name);
+            nameField.appendChild(itemName);
+            currentItem.appendChild(nameField);
+            
+            // Add price
+            var priceField = document.createElement('h4');
+            var itemPrice = document.createTextNode("€" + items[i].price);
+            priceField.appendChild(itemPrice);
+            currentItem.appendChild(priceField);
+
+            currentItem.classList.add("food-item");
+            topMealPackages.appendChild(currentItem);
+        }
+    };
+    
+    // Add meal packages to list
+    for (var i = 0; i < items.length ; i++) {
+        // Create new div for food item
+        var currentItem = document.createElement("div");
+
+        // Add image
+        var itemImg = document.createElement("img");
+        itemImg.src = items[i].imgName + ".jpg";
+        itemImg.classList.add("item-image");
+        currentItem.appendChild(itemImg);
+
+        // Add name
+        var nameField = document.createElement('h3');
+        var itemName = document.createTextNode(items[i].name);
+        nameField.appendChild(itemName);
+        currentItem.appendChild(nameField);
+        
+        // Add price
+        var priceField = document.createElement('h4');
+        var itemPrice = document.createTextNode("€" + items[i].price);
+        priceField.appendChild(itemPrice);
+        currentItem.appendChild(priceField);
+
+        currentItem.classList.add("food-item");
+        topMealPackages.appendChild(currentItem);
+    };
+
+});
---
+++ server.js
@@ -0,0 +1,51 @@
+var express = require("express");
+var app = express();
+
+// Require path module.
+var path = require("path");
+
+var HTTP_PORT = process.env.PORT || 8080;
+
+// This function will run when the server begins listening for requests.
+function onHttpStart() {
+    console.log("Server running on: " + HTTP_PORT);
+}
+
+app.use(express.static('public'));
+
+// ROUTES
+app.get("/", function(req,res) {
+    res.sendFile(path.join(__dirname,"/views/home.html"));
+});
+
+app.get("/login", function(req,res) {
+    res.sendFile(path.join(__dirname,"/views/login.html"));
+});
+
+app.get("/signup", function(req,res) {
+    res.sendFile(path.join(__dirname,"/views/signup.html"));
+});
+
+app.post("/signup", function(req, res) {
+    var reply='';
+    reply += req.body.fname;
+    reply += req.body.lname;
+    reply += req.body.email;
+    reply += req.body.pword;
+    res.send(reply);
+});
+
+app.post("/login", function(req, res) {
+    var reply='';
+    reply += req.body.email;
+    reply += req.body.pword;
+    res.send(reply);
+});
+
+app.get("/mealpackages", function(req,res) {
+    res.sendFile(path.join(__dirname,"/views/mealpackages.html"));
+});
+
+
+// Setup http server to listen on HTTP_PORT
+app.listen(HTTP_PORT, onHttpStart);
